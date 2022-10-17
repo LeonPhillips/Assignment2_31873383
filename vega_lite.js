4fe591945fabff260a7f0d9cfc0096e55eeb2768
LeonPhillips <97657604+LeonPhillips@users.noreply.github.com>
2022-10-17 17:11:03 +1100

    Add files via upload

---
+++ vega_lite.js
@@ -0,0 +1,23 @@
+var vg_1 = "interactive_disp.json";
+
+vegaEmbed("#map_bar",vg_1,{"actions":false}).then(function(result) {
+
+}).catch(console.error)
+
+
+//2
+
+var vg_2 = "other_props.json";
+
+vegaEmbed("#other_props_id",vg_2,{"actions":false}).then(function(result) {
+
+}).catch(console.error)
+
+
+//3 
+
+var vg_3 = "time_comp.json";
+
+vegaEmbed("#time_comp_id",vg_3,{"actions":false}).then(function(result) {
+
+}).catch(console.error)
